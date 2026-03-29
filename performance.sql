3992a70d44a4284d6768a70d9513ca5164855db1
mikedatasolutions-spec <mikedatasolutions@gmail.com>
2026-03-28 23:31:39 -0400

    Add indexes to improve query performance
    This script improves performance by creating indexes on frequently queried columns.

--- performance.sql
+++ performance.sql
@@ -1,1 +1,8 @@
 
+/*
+Extended Description:
+This script improves performance by creating indexes on frequently queried columns.
+*/
+
+CREATE INDEX idx_customer_id ON Orders(customer_id);
+CREATE INDEX idx_order_date ON Orders(order_date);
